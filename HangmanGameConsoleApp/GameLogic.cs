3abe45e1fb58c297ca5b23f2b176b909bded396d
Petra Rábai <petra.rabai@icloud.com>
2023-01-14 12:58:39 +0100

    Add project files.

---
+++ HangmanGameConsoleApp/GameLogic.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangmanGameConsoleApp
+{
+	public class GameLogic
+	{
+		public int playerLives = 5;
+		public string guessWord = "Startrek";
+
+		public void Game()
+		{
+			int guessWordCount = guessWord.Length;
+			char[] guessWordCharacters = new char[guessWordCount];
+			char[] displayedWord = new char[guessWordCount];
+			char[] result = new char[guessWordCount];
+
+			Console.WriteLine("Welcome to Hangmen Game!");
+			Console.WriteLine("Try to guess the word: ");
+			Console.WriteLine("Word count is " + guessWordCount);
+
+			for (int i = 0; i < guessWordCharacters.Length; i++)
+			{
+				Console.Write("_ ");
+				guessWordCharacters[i] = guessWord[i];
+			}
+			Console.WriteLine("\n");
+			while (playerLives != 0)
+			{
+				Console.WriteLine("\n\nAdd a letter: \n");
+				char playerguessChar = Convert.ToChar(Console.ReadLine());
+				Console.WriteLine("\n");
+
+				for (int i = 0; i < guessWordCharacters.Length; i++)
+				{
+					if (guessWordCharacters[i] == playerguessChar)
+					{
+						displayedWord[i] = playerguessChar;
+						
+					}
+					else
+					{
+						displayedWord[i] = '_';
+						
+					}
+	
+				}
+
+				for (int i = 0; i < displayedWord.Length; i++)
+				{
+
+					Console.Write(displayedWord[i]);
+				}
+				result = displayedWord;
+
+				Console.WriteLine("\n");
+			}
+			
+			if (playerLives == 0)
+			{
+				Console.WriteLine("You lost!\n The word is: " + guessWord);
+			}
+			else
+			{
+				Console.WriteLine("You win! :)");
+			}
+		}
+	}
+}
---
+++ HangmanGameConsoleApp/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HangmanGameConsoleApp;
+
+GameLogic gameLogic = new GameLogic();
+gameLogic.Game();
+Console.WriteLine("Hello, World!");
